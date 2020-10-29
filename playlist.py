0ff2abc353e37fdd87c2996a5a3ea4cc85507a20
shaunnorton <shaunnorton87@gmail.com>
2020-10-29 13:49:03 -0700

    refactored to make a double linked list and tested

--- playlist.py
+++ playlist.py
@@ -2,6 +2,7 @@ class Song:
     def __init__(self,data):
         self.data = data
         self.next = None
+        self.previous = None
 
 class PlayList():
     def __init__(self):
@@ -13,6 +14,7 @@ class PlayList():
         new_song = Song(data)
         if self.tail is not None:
             self.tail.next = new_song
+            new_song.previous = self.tail
         self.tail = new_song
         if self.head is None:
             self.head = new_song
@@ -20,6 +22,8 @@ class PlayList():
     def prepend(self,data):
         new_song = Song(data)
         new_song.next = self.head
+        if self.head is not None:
+            self.head.previous = new_song
         self.head = new_song
         if self.tail is None:
             self.tail = new_song
@@ -33,3 +37,12 @@ class PlayList():
         if self.head is None:
             print("Your PlayList is Empty")
         print("__________________END OF YOUR PLAYLIST__________________")
+
+    def delete_from_head(self):
+        self.head = self.head.next
+        self.head.previous = None
+
+
+    def delete_from_tail(self):
+        self.tail.previous.next = None
+        self.tail = self.tail.previous
